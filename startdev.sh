1838a0a2fd8a75dee6e997fa48250458d9b695f3
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-10-26 16:37:53 +0100

    chore: refine the script and start the development workflow

--- startdev.sh
+++ startdev.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/sh
 
+# start the virtual environment
+source venv/bin/activate
+
+# setup the environment variables
 export FLASK_APP=flaskr
 export FLASK_ENV=development
+
+# and run the development server
 flask run
