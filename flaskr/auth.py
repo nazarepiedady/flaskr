771bc940d71579119e4ac1d5ac7c99b1117f3aea
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-10-27 18:07:20 +0100

    feat: create the `register` view

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,3 +9,31 @@ from flaskr.db import get_database
 
 
 blueprint = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@blueprint.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        database = get_database()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                database.execute(
+                    'INSERT INTO user (username, password) VALUES (?, ?)',
+                    (username, generate_password_hash(password)),
+                )
+                database.commit()
+            except database.IntegrityError:
+                error = f'User {username} is already registered.'
+            else:
+                return redirect(url_for('auth.login'))
+        flash(error)
+    return render_template('auth/register.html')
