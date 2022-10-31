915dda9419632f34a476ceddd51d34363a9c04de
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-10-31 17:50:25 +0100

    feat: create the `/create` route to write and publish new posts

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -19,3 +19,28 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
+
+
+@blueprint.route('/create')
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            database = get_database()
+            database.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            database.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
