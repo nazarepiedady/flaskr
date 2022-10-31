a759c0a13f8567f5dcea74a9c459f0c4121aaad0
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-10-31 13:44:09 +0100

    feat: create the route `/` as the `index` of the blog

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -8,3 +8,14 @@ from flaskr.db import get_database
 
 
 blueprint = Blueprint('blog', __name__)
+
+
+@blueprint.route('/')
+def index():
+    database = get_database()
+    posts = database.execute(
+        'SELECT post.id, title, body, created, author_id, username'
+        ' FROM post JOIN user ON post.author_id = user.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
