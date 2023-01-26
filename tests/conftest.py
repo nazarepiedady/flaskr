acc29bfde05bccf7d9002b21d85e505188d91727
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-01-26 20:22:01 +0000

    test: write runner fixture to return the test cli runner resources

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,3 +32,8 @@ def application():
 @pytest.fixture
 def client(application):
     return application.test_client()
+
+
+@pytest.fixture
+def runner(application):
+    return application.test_cli_runner()
