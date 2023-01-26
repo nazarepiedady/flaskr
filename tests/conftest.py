498f8d75aefe5e97c6816c206758ba5197b797c9
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-01-26 20:18:30 +0000

    test: write application fixture to configure test environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,22 @@ from flaskr.db import get_database, init_database
 
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as file:
     __data_sql = file.read().decode('utf-8')
+
+
+@pytest.fixture
+def application():
+    database_file_directory, database_path = tempfile.mkstemp()
+
+    application = create_app({
+        'TESTING': True,
+        'DATABASE': database_path,
+    })
+
+    with application.app_context():
+        init_database()
+        get_database().executescript(__data_sql)
+
+    yield application
+
+    os.close(database_file_directory)
+    os.unlink(database_path)
