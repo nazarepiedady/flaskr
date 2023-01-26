fc91a8af5c16abcb6d4315992a2870205f6218f8
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-01-26 19:52:29 +0000

    chore: update format of the `startserver.sh`

--- startserver.sh
+++ startserver.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sh
 
 # start the server
-if [ "$VIRTUAL_ENV" ]
-then flask --app flaskr --debug run
+if [ "$VIRTUAL_ENV" ]; then
+  flask --app flaskr --debug run
 else
   source "venv/bin/activate"
   flask --app flaskr --debug run
