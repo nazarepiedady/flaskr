967167f8623a3ba938b42f29b6bcb80247aae1df
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-02-04 16:53:55 +0000

    test: write test for the `/hello` route

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -5,3 +5,8 @@ def test_config():
     ''' test configuration '''
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
+
+def test_hello(client):
+    ''' test the /hello route path '''
+    response = client.get('/hello')
+    assert response.data == b'Hello, World!'
