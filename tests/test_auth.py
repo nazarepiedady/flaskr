f9d4f7371123417bfe396564053204f448e16d2b
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-02-04 17:40:59 +0000

    test: write for the register validate input

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -17,3 +17,16 @@ def test_register(client, application):
         assert get_database().execute(
             "SELECT * FROM user WHERE username = 'a'",
         ).fetchone() is not None
+
+@pytest.mark.parametrize(('username', 'password', 'message'),(
+    ('', '', b'Username is required.'),
+    ('a', '', b'Password is required.'),
+    ('test', 'test', b'already registered'),
+))
+def test_register_validate_input(client, username, password, message):
+    ''' test the register validade input '''
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
+    )
+    assert message in response.data
