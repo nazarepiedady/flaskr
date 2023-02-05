e21080ce29071d655c2701caf3e8c0ae2e4ce5aa
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-02-05 08:43:56 +0000

    test: write for the index view and some behaviors

---
+++ tests/test_blog.py
@@ -0,0 +1,17 @@
+import pytest
+from flaskr.db import get_database
+
+
+def test_index(client, authentication):
+    ''' test the index route '''
+    response = client.get('/')
+    assert b'Log In' in response.data
+    assert b'Register' in response.data
+
+    authentication.login()
+    response = client.get('/')
+    assert b'Log Out' in response.data
+    assert b'test title' in response.data
+    assert b'by test on 2018-01-01' in response.data
+    assert b'test\nbody' in response.data
+    assert b'href="/1/update"' in response.data
